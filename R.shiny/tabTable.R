9bb82c34dc00c609bc56a7d85aa749734290f2fd
Helga Ingimundardottir <helga85@gmail.com>
2015-04-23 13:05:23 +0000

    Conditional panels

--- R.shiny/tabTable.R
+++ R.shiny/tabTable.R
@@ -1,11 +1,16 @@
 output$tabTable <- renderUI({
   dashboardBody(
     fluidRow(
-      box(
+      box(title="Settings", collapsible = T,
         selectInput("rawData", "Table:", c("OPT","SDR")),
-        radioButtons("summary.var", "Statistics for", c("Rho","Makespan","Optimum")),
-        selectInput("variable", "Summarise over variables:", c("Problem","Dimension","Set","SDR"),multiple=TRUE))
-    ),
+        conditionalPanel("input.rawData == 'SDR'",
+                         radioButtons("summary.var.sdr", "Statistics for", c("Rho","Makespan")),
+                         selectInput("variable.sdr", "Summarise over variables:", c("Problem","Dimension","Set","SDR"),multiple=TRUE)),
+
+        conditionalPanel("input.rawData == 'OPT'",
+                         radioButtons("summary.var.opt", "Statistics for", c("Optimum")),
+                         selectInput("variable.opt", "Summarise over variables:", c("Problem","Dimension","Set"),multiple=TRUE))
+    )),
     fluidRow(dataTableOutput("stats.table"))
   )
 })
@@ -16,10 +21,16 @@ output$stats.table <- renderDataTable({
                'SDR'=dataset.SDR(),
                'OPT'=subset(dataset.OPT,Problem %in% input$problems & Dimension %in% input$dimension))
 
-  vars = input$variable
+  vars = switch(input$rawData,
+                'SDR'=input$variable.sdr,
+                'OPT'=input$variable.opt)
+
   if(is.null(vars)) { return(dat) }
   vars = vars[vars %in% colnames(dat)]
-  over.var=input$summary.var
+  over.var=switch(input$rawData,
+                  'SDR'=input$summary.var.sdr,
+                  'OPT'=input$summary.var.opt)
+
   if(!any(over.var %in% colnames(dat))) { return(dat) }
 
   stat=ddply(dat,.variables = vars, function(X) data.frame(as.list(summary(X[,over.var])), Cnt=nrow(X)))
