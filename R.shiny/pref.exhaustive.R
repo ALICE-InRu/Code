2692a8d02e6bade234d68294cda0c18abeaf0dc0
Helga Ingimundardóttir <helga85@gmail.com>
2015-08-08 14:50:18 +0000

    Set stepwise optimality accuracy for CDR models

--- R.shiny/pref.exhaustive.R
+++ R.shiny/pref.exhaustive.R
@@ -202,7 +202,7 @@ get.optAccuracy <- function(model,reportMean=T){
   m=regexpr(".(?<Dimension>[0-9]+x[0-9]+).",model,perl=T)
   dim=getAttribute(model,m,'Dimension')
   fname=paste0(DataDir,'Stepwise/accuracy/',paste0(model,'.csv'))
-  if(!file.exists(fname)){ return(NULL)}
+  if(!file.exists(fname)){ set.optAccuracy(model) }
   acc = read_csv(fname)
 
   m=regexpr("F(?<NrFeat>[0-9]+).M(?<Model>[0-9]+)", acc$CDR, perl=T)
@@ -224,6 +224,46 @@ get.optAccuracy <- function(model,reportMean=T){
   }
   return(acc)
 }
+
+set.optAccuracy <- function(model){
+
+  fname=paste0(DataDir,'Stepwise/accuracy/',paste0(model,'.csv'))
+
+  m=regexpr('exhaust.(?<Problem>[a-z].[a-z]+).(?<Dimension>[0-9]+x[0-9]+).(?<Rank>[a-z]).(?<Track>[A-Z]+)',model,perl=T)
+  dim=getAttribute(model,m,'Dimension')
+  problem=getAttribute(model,m,'Problem')
+  rank=getAttribute(model,m,'Rank')
+  track=getAttribute(model,m,'Track')
+
+  if(file.exists(fname)){ opt.acc=read.csv(fname)}else{
+    print(fname)
+    weights = get.prefWeights(model,grepl(model,'timedependent'),T)
+    trdat = get.files.TRDAT(problem,dim,track,rank)
+    trdat$isOPT=trdat$Rho==0
+    Ntrain=quantile(unique(trdat$PID),0.8)
+    trdat=subset(trdat,PID>Ntrain) # only do this for validation set
+    trdat=trdat[,grep('phi|PID|Step|isOPT',colnames(trdat))]
+    phis=colnames(weights)
+    trdat=cbind(trdat[,c('Step','PID','isOPT')],as.matrix(trdat[,phis]) %*% t(weights))
+    trdat=melt(trdat,id.vars = colnames(trdat)[grep('^F[0-9]+',colnames(trdat),invert = T)])
+    mdat=NULL
+    for(var in unique(trdat$variable)){ print(var)
+      tmp=ddply(subset(trdat,variable==var),~PID+Step+variable,mutate,isMax=value==max(value),.progress = 'text')
+      tmp=subset(tmp,isMax==T)
+      tmp=ddply(tmp,~Step+variable,summarise,Validation.Accuracy=mean(isOPT),.progress = 'text')
+      mdat=rbind(mdat,tmp)
+    }
+    opt.acc=dcast(mdat,variable~Step,value.var='Validation.Accuracy')
+    steps=2:ncol(opt.acc)
+    colnames(opt.acc)[steps]=paste('Step',colnames(opt.acc)[steps],sep='.')
+    opt.acc[,steps]=round(opt.acc[,steps],digit=2)
+    write.csv(opt.acc,file=fname,row.names=F,quote=F)
+  }
+  return(opt.acc)
+}
+
+
+
 
 get.prefSummary <- function(problems,dim,track,rank,timedependent,bias){
 
