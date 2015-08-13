bd773b51048240838731902ea552923130894f35
Helga Ingimundardóttir <helga85@gmail.com>
2015-08-13 23:23:03 +0000

    Duplicated plot names fixed

--- R.shiny/tabPref.stepwiseBias.R
+++ R.shiny/tabPref.stepwiseBias.R
@@ -2,21 +2,21 @@ output$tabPref.stepwiseBias <- renderUI({
   dashboardBody(
     fluidRow(
       box(title='Boxplot', width=12,
-          plotOutput('plot.stepwiseBias', height=500)),
+          plotOutput('plot.CDR.stepwiseBias', height=500)),
       box(title='Summary for Rho', width=9,
-          tableOutput('table.stepwiseBias'))
+          tableOutput('table.CDR.stepwiseBias'))
     )
   )
 })
 
 CDR.stepwiseBias <- reactive({ get.CDR.stepwiseBias(input$problems,input$dimension) })
 
-output$plot.stepwiseBias <- renderPlot({
+output$plot.CDR.stepwiseBias <- renderPlot({
   withProgress(message = 'Plotting boxplots', value = 0, {
     plot.CDR.stepwiseBias(CDR.stepwiseBias())
   })
 })
 
-output$table.stepwiseBias <- renderTable({
+output$table.CDR.stepwiseBias <- renderTable({
   table.CDR.stepwiseBias(CDR.stepwiseBias())
 }, include.rownames=FALSE, sanitize.text.function=function(x){x})
