5119415fb05c6378d240bdaf172c53730451bc7e
Helga Ingimundardóttir <helga85@gmail.com>
2015-09-08 10:34:47 +0000

    K-S test

--- R.shiny/feat.footprints.R
+++ R.shiny/feat.footprints.R
@@ -18,14 +18,18 @@ ks.matrix.stepwise <- function(df, bonferroniAdjust=T)
   rownames(ks.matrix)=values
   colnames(ks.matrix)=1:TotalSteps
 
-  op <- options(warn = (-1)) # suppress warnings
+  conf.level=0.95
+  alpha=1-conf.level
+  if(bonferroniAdjust){
+    alpha=alpha/TotalSteps
+  }
 
   for(step in 1:TotalSteps){
     ddf1=subset(df1, Step==step)
     ddf2=subset(df2, Step==step)
     if(nrow(ddf1)>0 & nrow(ddf2)>0){
       for(col in values){
-        ks.matrix[col,step] <- ks.test(ddf1[,col],ddf2[,col])$p
+        ks.matrix[col,step] <- ks.test2(ddf1[,col],ddf2[,col],alpha)
       }
     } else { ks.matrix[col,step]=NA }
   }
@@ -33,29 +37,15 @@ ks.matrix.stepwise <- function(df, bonferroniAdjust=T)
   N1=nrow(subset(df1,Step==1))
   N2=nrow(subset(df2,Step==1))
 
-  conf.level=0.95
-  if(bonferroniAdjust){
-    alpha=1-conf.level
-    conf.level = 1-alpha/TotalSteps
-  }
-
-  sign.ks = abs(ks.matrix) > conf.level # Kolmogorov-Smirnov test: a two-sample test of the null
-  #hypothesis that 'Easy' and 'Hard' were drawn from the same continuous distribution is performed.
-  sign.ks <- melt(sign.ks)
-  colnames(sign.ks)=c('Feature','Step','Significant')
-
-  ks.matrix <- round(ks.matrix, digits = 2)
   ks.dat <- melt(ks.matrix)
-  colnames(ks.dat)=c('Feature','Step','Kolmogorov-Smirnov')
-
-  ks.dat <- merge(ks.dat, sign.ks, by=c('Feature','Step'))
+  colnames(ks.dat)=c('Feature','Step','Significant')
 
   if(any(is.na(ks.dat$Significant))){ ks.dat <- ks.dat[-which(is.na(ks.dat$Significant)),] }
   if(nrow(ks.dat)>0) {
     ks.dat$N.Easy = N1
     ks.dat$N.Hard = N2
     ks.dat$Track = 'ALL'
-    ks.dat$Difficulty = 'Easy vs. Hard'
+    ks.dat$Difficulty = 'Reject null hypothesis'
   }
 
   return(ks.dat)
@@ -134,7 +124,7 @@ plot.stepwise.test <- function(df){
   df$Feature <- factor(df$Feature, levels=rev(levels(df$Feature)))
 
   df = factorTrack(df)
-  df$Difficulty=factor(df$Difficulty, levels=c('Easy','Easy vs. Hard','Hard'))
+  df$Difficulty=factor(df$Difficulty)
 
   p=ggplot(df, aes(x=Step, y=Feature)) + facet_wrap(~Track,nrow=2) +
     geom_point(data=subset(df,Significant==T), aes(Step, Feature, shape = Difficulty), color = 'black') +
@@ -149,6 +139,8 @@ plot.correlation.matrix.stepwise <- function(cor.df){
 }
 
 plot.ks.matrix.stepwise <- function(ks.df){
-  plot.stepwise.test(ks.df) + scale_shape_manual(values=c(8))+ylab('Kolmogorov-Smirnov Test')
+  plot.stepwise.test(ks.df) + scale_shape_manual(
+    expression(H[0] *~ ': Easy and Hard are drawn from the same continuous distribution'),
+    values=c(8))+ylab('Kolmogorov-Smirnov Test')
 }
 
--- R.shiny/global.R
+++ R.shiny/global.R
@@ -21,5 +21,13 @@ rhoLabel=expression("Deviation from optimality," * ~ rho * ~ " (%)")
 dataset.OPT=get.files.OPT()
 dataset.SDR=get.files.SDR()
 
-
-
+ks.test2 <- function(x1,x2,alpha=0.05){
+  # H = KSTEST2(X1,X2) performs a Kolmogorov-Smirnov (K-S) test
+  # to determine if independent random samples, X1 and X2, are drawn from the
+  # same underlying continuous population. H indicates the result of the hypothesis test:
+  #       H = 0 => Do not reject the null hypothesis at the 5% significance level.
+  #       H = 1 => Reject the null hypothesis at the 5% significance level.
+  pValue <- ks.test(x1,x2)$p
+  H  =  (alpha >= pValue)
+  return(H)
+}
